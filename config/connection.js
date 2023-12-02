a03ae32ad1dcab8052fbaafe0a7ab8473fb7291e
Diego Yavara <diego.yavara98@gmail.com>
2023-12-02 15:45:06 -0700

    trying to fix the code so it deployes in heroku

--- config/connection.js
+++ config/connection.js
@@ -4,8 +4,13 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
+    // Use JawsDB URL if available (Heroku)
     sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else if (process.env.DATABASE_URL) {
+    // Use DATABASE_URL if available (Heroku)
+    sequelize = new Sequelize(process.env.DATABASE_URL);
 } else {
+    // Use local MySQL configuration
     sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
         host: 'localhost',
         dialect: 'mysql',
